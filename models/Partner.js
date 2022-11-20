0df396d5a0ec61995d0934c93a2cb5fc19d5a009
Stoyann Velten <stoyann.openclassrooms@gmail.com>
2022-11-21 00:12:48 +1100

    creating partner models

---
+++ models/Partner.js
@@ -0,0 +1,96 @@
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+const PartnerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add name"],
+      unique: true,
+      trim: true,
+      maxlength: [50, "Name can not be more than 50 characters"],
+    },
+    slug: String,
+    description: {
+      type: String,
+      required: [true, "Please add description"],
+      maxlength: [500, "Name can not be more than 500 characters"],
+    },
+    website: {
+      type: String,
+      match: [
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        "Please use a valid URL with HTTP or HTTPS",
+      ],
+    },
+    phone: {
+      type: String,
+      maxlength: [20, "Phone number can not be longer than 20 characters"],
+    },
+    email: {
+      type: String,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
+    address: {
+      type: String,
+      required: [true, "Please add an address"],
+    },
+    location: {
+      // GeoJSON Point
+      type: {
+        type: String,
+        enum: ["Point"],
+      },
+      coordinates: {
+        type: [Number],
+        index: "2dsphere",
+      },
+      formattedAddress: String,
+      street: String,
+      city: String,
+      state: String,
+      zipcode: String,
+      country: String,
+    },
+    type: {
+      // Array of strings
+      type: [String],
+      required: true,
+      enum: [
+        "Association",
+        "Revendeur",
+        "Fournisseur",
+        "Institution",
+        "Autres",
+      ],
+    },
+    photo: {
+      type: String,
+      default: "no-photo.jpg",
+    },
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+    // user: {
+    //   type: mongoose.Schema.ObjectId,
+    //   ref: "User",
+    //   required: true,
+    // },
+  },
+  { timestamps: true },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// create Partner slug from the name
+PartnerSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+module.exports = mongoose.model("Partner", PartnerSchema);
--- server.js
+++ server.js
@@ -1,30 +1,45 @@
+// dependency
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require("colors");
 //import middlewares
-const morgan = require('morgan')
-
-// Route files
-const partners = require("./routes/partners");
+const morgan = require("morgan");
+// load config DB
+const connectDB = require("./config/db");
 
 //load environement variables
 dotenv.config({ path: "./config/config.env" });
 
+//Connect to database
+connectDB();
+
+// Route files
+const partners = require("./routes/partners");
 
 // initialize express  application
 const app = express();
 
 // Dev logging Middleware
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
-
 
 //Mount routers
 app.use("/krysto/api/v1/partners", partners);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT} `.white
+      .underline.bold.bgGreen
+  )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
