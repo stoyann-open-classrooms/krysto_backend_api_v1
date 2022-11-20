77c96f9a18f4ab84f49340793a35211917d34cb7
Stoyann Velten <stoyann.openclassrooms@gmail.com>
2022-11-21 03:46:18 +1100

    route for getting a partners in radius from zipcode

--- controllers/partners.js
+++ controllers/partners.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middlewares/async");
+const geocoder = require('../utils/geocoder');
 const Partner = require("../models/Partner");
 
 //@description:     Get all partners
@@ -15,60 +16,82 @@ exports.getPartners = asyncHandler(async (req, res, next) => {
 //@description:     Get a single partner
 //@ route:          GET /krysto/api/v1/partners/:id
 //@access:          Public
-exports.getPartner = asyncHandler( async (req, res, next) => {
-  
-    const partner = await Partner.findById(req.params.id);
-    if (!partner) {
-      return next(
-        new ErrorResponse(`Partner not found with ID of ${req.params.id}`, 404)
-      );
-      }
-    res.status(200).json({ success: true, data: partner });
-  
+exports.getPartner = asyncHandler(async (req, res, next) => {
+  const partner = await Partner.findById(req.params.id);
+  if (!partner) {
+    return next(
+      new ErrorResponse(`Partner not found with ID of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({ success: true, data: partner });
 });
 
 //@description:     Create a partner
 //@ route:          POST /krysto/api/v1/partners
 //@access:          Private
 exports.createPartner = asyncHandler(async (req, res, next) => {
-  
-    const partner = await Partner.create(req.body);
-    res.status(201).json({
-      success: true,
-      data: partner,
-    });
- 
+  const partner = await Partner.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: partner,
+  });
 });
 
 //@description:     Update a partner
 //@ route:          PUT /krysto/api/v1/partners/:id
 //@access:          Private
 exports.updatePartner = asyncHandler(async (req, res, next) => {
- 
-    const partner = await Partner.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!partner) {
-      return next(
-        new ErrorResponse(`Partner not found with ID of ${req.params.id}`, 404)
-      );
-    }
-    res.status(200).json({ success: true, data: partner });
-  
+  const partner = await Partner.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!partner) {
+    return next(
+      new ErrorResponse(`Partner not found with ID of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({ success: true, data: partner });
 });
 
 //@description:     Delete a partner
 //@ route:          DELETE /krysto/api/v1/partners/:id
 //@access:          Private
-exports.deletePartner = asyncHandler( async (req, res, next) => {
-  
-    const partner = await Partner.findByIdAndDelete(req.params.id);
-    if (!partner) {
-      return next(
-        new ErrorResponse(`Partner not found with ID of ${req.params.id}`, 404)
-      );
-    }
-    res.status(200).json({ success: true, data: {} });
+exports.deletePartner = asyncHandler(async (req, res, next) => {
+  const partner = await Partner.findByIdAndDelete(req.params.id);
+  if (!partner) {
+    return next(
+      new ErrorResponse(`Partner not found with ID of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({ success: true, data: {} });
+});
 
+
+
+
+// @description      Get partners within a radius
+// @route            GET /api/v1/partners/radius/:zipcode/:distance
+// @access           Private
+exports.getPartnersInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
+
+  // Get lat/lng from geocoder
+  const loc = await geocoder.geocode(zipcode);
+  const lat = loc[0].latitude;
+  const lng = loc[0].longitude;
+
+  // Calc radius using radians
+  // Divide dist by radius of Earth
+  // Earth Radius =  6,378 km
+  const radius = distance / 6378;
+
+  const partners = await Partner.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+  });
+
+  res.status(200).json({
+    success: true,
+    count: partners.length,
+    data: partners
+  });
 });
--- routes/partners.js
+++ routes/partners.js
@@ -1,13 +1,18 @@
 const express = require("express");
 // get controller function
-const{ getPartners, getPartner, createPartner, updatePartner, deletePartner  } = require('../controllers/partners')
+const {
+  getPartners,
+  getPartner,
+  createPartner,
+  updatePartner,
+  deletePartner,
+  getPartnersInRadius,
+} = require("../controllers/partners");
 
+const router = express.Router();
 
-const router = express.Router()
+router.route("/radius/:zipcode/:distance").get(getPartnersInRadius);
+router.route("/").get(getPartners).post(createPartner);
+router.route("/:id").get(getPartner).put(updatePartner).delete(deletePartner);
 
-router.route('/').get(getPartners).post(createPartner)
-router.route('/:id').get(getPartner).put(updatePartner).delete(deletePartner)
-
-  module.exports = router;
-  
-  
+module.exports = router;
